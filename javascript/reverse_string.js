227a226622ddca45add22380d9de451f6c2ceec7
John <johnfarris217@gmail.com>
2023-01-22 13:24:57 -0600

    initial solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,29 @@
 function reverseString(str) {
   // type your code here
+  let ans = ''
+  for (let i = 0; i < str.length; i++) {
+    ans = str[i] + ans
+  }
+  return ans
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'sllab'");
+  console.log("=>", reverseString("balls"));
+
+  console.log("");
+
+  console.log("Expecting: 'allets'");
+  console.log("=>", reverseString("stella"));
+
+  console.log("");
+
+  console.log("Expecting: 'lacitpeks'");
+  console.log("=>", reverseString("skeptical"));
+
+  console.log("");
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
